47dbe8f225fa0ad1dc7271c5329ba2d8943edb71
Laurel Perkins <lmperkins20@gmail.com>
2023-04-11 10:51:14 -0700

    lab-06 complete, more or less (less)

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,66 +1,31 @@
-'use strict'
+'use strict';
 
-// // from the in-class demo:
-// UserModel.beforeCreate(async user => {
-//   user.password = await bcrypt.hash(user.password, 10);
-// });
-// // this is done below in the signup function, just slightly different
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../models/');
+const router = express.Router();
+const basicAuth = require('./basic');
 
-// Signup Route -- create a new user
-// Two ways to test this route with httpie
-// echo '{"username":"john","password":"foo"}' | http post :3000/signup
-// http post :3000/signup username=john password=foo
-app.post('/signup', async (req, res) => {
+router.post('/signup', createUser);
+router.post('/signin', [basicAuth, signinUser]);
 
-  try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    const record = await Users.create(req.body);
-    res.status(200).json(record);
-  } catch (e) { res.status(403).send('Error Creating User'); }
-});
-
-// // from in-class demo
-// app.post('/signup', async (request, response, next) => {
-//   let newUser = await UserModel.create(request.body);
-//   response.json(newUser);
-// });
-
-// Signin Route -- login with username and password
-// test with httpie
-// http post :3000/signin -a john:foo
-app.post('/signin', async (req, res) => {
-
-  /*
-    req.headers.authorization is : "Basic am9objpmb28="
-    To get username and password from this, take the following steps:
-      - Turn that string into an array by splitting on ' '
-      - Pop off the last value
-      - Decode that encoded string so it returns to user:pass
-      - Split on ':' to turn it into an array
-      - Pull username and password from that array
-  */
-
-  let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'am9objpmb28=']
-  let encodedString = basicHeaderParts.pop();  // am9objpmb28=
-  let decodedString = base64.decode(encodedString); // "username:password"
-  let [username, password] = decodedString.split(':'); // username, password
-
-  /*
-    Now that we finally have username and password, let's see if it's valid
-    1. Find the user in the database by username
-    2. Compare the plaintext password we now have against the encrypted password in the db
-       - bcrypt does this by re-encrypting the plaintext password and comparing THAT
-    3. Either we're valid or we throw an error
-  */
-  try {
-    const user = await Users.findOne({ where: { username: username } });
-    const valid = await bcrypt.compare(password, user.password);
-    if (valid) {
-      res.status(200).json(user);
+async function createUser(req, res) {
+    try {
+        req.body.password = await bcrypt.hash(req.body.password, 10);
+        const record = await User.create(req.body);
+        console.log(record);
+        res.status(200).json(record);
+    } catch (e) { 
+        res.status(403).send('Error Creating User'); 
     }
-    else {
-      throw new Error('Invalid User');
-    }
-  } catch (error) { res.status(403).send('Invalid Login'); }
+}
 
-});
+async function signinUser(req, res, next) {
+    try {
+        res.status(200).send('Login successful!');
+    } catch (e) {
+        res.status(401).send('Invalid credentials');
+    }
+}
+
+module.exports = router;
--- src/middleware/404.test.js
+++
@@ -1,2 +0,0 @@
-'use strict'
-// unit tests
